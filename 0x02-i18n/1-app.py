86068d8fc112bbfc936b572ccb6550dd637c0411
StevenAwo <rotimiawomodu@gmail.com>
2024-05-14 18:55:47 +0100

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
-""" Th task for 1-app's module """
+""" 1-app module """
 
 from flask import Flask, render_template
 
 from flask_babel import Babel
 
-from routes.routes_1 import app_routes as app_routes_1
-
 app = Flask(__name__)
+
+# Instantiate the Babel object
 babel = Babel(app)
 
 class Config(object):
-    """Configuring class thats for babel"""
+    """Configng class thats for Babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+# Use Config as config for the Flask app
 app.config.from_object(Config)
 
 @app.route('/', methods=["GET"], strict_slashes=False)
 def home():
-    """ To the Home page """
+    """The Home's page """
     return render_template('1-index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
+
