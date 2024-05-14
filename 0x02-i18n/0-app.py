d442068a2356036b7afaf3cfa3b4a73867c4b466
StevenAwo <rotimiawomodu@gmail.com>
2024-05-14 17:36:34 +0100

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python3
-""" 0-app module """
-from flask import Flask
-from routes.routes_0 import app_routes
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.register_blueprint(app_routes)
+@app.route('/')
+def index():
+    return render_template('0-index.html')
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run()
