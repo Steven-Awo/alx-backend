b3128dc0fb265b9f3956bce9e5ad5fe5b74863f1
StevenAwo <rotimiawomodu@gmail.com>
2024-05-14 22:15:15 +0100

    task 5

---
+++ 0x02-i18n/routes/5-routes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""tHE route FOR task 5"""
+
+from typing import Union
+
+from flask import Blueprint, render_template, g
+
+
+app_routes = Blueprint('app_routes', __name__)
+
+
+@app_routes.route('/', methods=["GET"], strict_slashes=False)
+def home():
+    """The Home's page """
+    return render_template('5-index.html', user=g.user)
