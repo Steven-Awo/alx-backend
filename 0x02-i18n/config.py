2e7c3dc3f837176cc33a06a31093a78ad3171776
StevenAwo <rotimiawomodu@gmail.com>
2024-05-14 17:14:27 +0100

    task 0

---
+++ 0x02-i18n/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" ConfigURING THE model thats for babel """
+
+
+class Config(object):
+    """Configurin class thats for babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
