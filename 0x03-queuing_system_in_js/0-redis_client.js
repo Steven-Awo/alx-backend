5655bc16f93441a137d88dc3928fc4d480d4f72b
StevenAwo <rotimiawomodu@gmail.com>
2024-06-10 21:49:41 +0100

    TASK 1

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,20 +1,29 @@
 import { createClient } from 'redis';
 
 async function main() {
+
   const client = createClient();
 
   client.on('connect', function() {
+
     console.log('Redis client connected to the server');
+
   });
 
   client.on('error', function(err) {
+
     console.log('Redis client not connected to the server:', err.message);
+
   });
 
   try {
+
     await client.connect();
+
   } catch (err) {
+
     console.log('Redis client not connected to the server:', err.message);
+
   }
 }
 
