393f2d75987b74cabe590035d7812d525be2e6c9
StevenAwo <rotimiawomodu@gmail.com>
2024-06-11 17:30:35 +0100

    TASK 6

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,29 @@
+#!/usr/bin/yarn dev
+import { createQueue } from 'kue';
+
+const queuee = createQueue({name: 'push_notification_code'});
+
+const job_to_create = queuee.create('push_notification_code', {
+
+  phoneNumber: '07045679939',
+  message: 'Account registered',
+
+});
+
+job_to_create
+  .on('enqueue', () => {
+
+    console.log('Notification job_to_create created:', job_to_create.id);
+
+  })
+  .on('complete', () => {
+
+    console.log('Notification job_to_create completed');
+
+  })
+  .on('failed attempt', () => {
+
+    console.log('Notification job_to_create failed');
+
+  });
+job_to_create.save();
